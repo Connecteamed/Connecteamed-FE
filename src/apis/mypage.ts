040bb7a424e9cb49e2c700b642fac25328de6142
hansol65 <whgksthf65@gmail.com>
2026-01-29 21:39:50 +0900

    ✨feat: 마이페이지 API 로직 작성

---
+++ src/apis/mypage.ts
@@ -0,0 +1,61 @@
+import type { ApiResponse } from '@/types';
+import type { MyProjectsData, RetrospectivesData } from '@/types/mypage';
+import axios from 'axios';
+
+const API_URL = 'https://api.connecteamed.shop';
+
+const authHeaders = () => {
+  const token = localStorage.getItem('accessToken');
+  return {
+    Authorization: token ? `Bearer ${token}` : '',
+  };
+};
+
+/**
+ * 완료한 프로젝트 목록 조회
+ */
+export const getMyProjects = async () => {
+  const res = await axios.get<ApiResponse<MyProjectsData>>(
+    `${API_URL}/api/mypage/projects/completed`,
+    {
+      headers: authHeaders(),
+    },
+  );
+  return res.data;
+};
+
+/**
+ * 완료한 프로젝트 삭제
+ */
+export const deleteProject = async (projectId: number) => {
+  const res = await axios.delete<ApiResponse<null>>(`${API_URL}/api/mypage/project/${projectId}`, {
+    headers: authHeaders(),
+  });
+  return res.data;
+};
+
+/**
+ * 나의 회고 목록 조회
+ */
+export const getMyRetrospectives = async () => {
+  const res = await axios.get<ApiResponse<RetrospectivesData>>(
+    `${API_URL}/api/mypage/retrospectives`,
+    {
+      headers: authHeaders(),
+    },
+  );
+  return res.data;
+};
+
+/**
+ * 회고 삭제
+ */
+export const deleteRestrospective = async (retrospectiveId: number) => {
+  const res = await axios.delete<ApiResponse<null>>(
+    `${API_URL}/api/mypage/retrospective/${retrospectiveId}`,
+    {
+      headers: authHeaders(),
+    },
+  );
+  return res.data;
+};
