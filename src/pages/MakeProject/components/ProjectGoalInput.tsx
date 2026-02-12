bb0151fc8bcbc88bf766332698f0b2378ae8f7f8
kiyeonkimm <paynjoy2@naver.com>
2026-02-12 21:57:55 +0900

    🐛Fix: 프로젝트 생성 QA 반영 및 새로고침 없이 반영 로직으로 변경

--- src/pages/MakeProject/components/ProjectGoalInput.tsx
+++ src/pages/MakeProject/components/ProjectGoalInput.tsx
@@ -5,21 +5,14 @@ type ProjectGoalInputProps = {
   onChange: (value: string) => void;
 };
 
-const ProjectGoalInput = ({
-  value,
-  currentLength,
-  maxLength,
-  onChange,
-}: ProjectGoalInputProps) => {
+const ProjectGoalInput = ({ value, currentLength, maxLength, onChange }: ProjectGoalInputProps) => {
   return (
     <div className="flex flex-col gap-1.5">
-      <div className="text-sm font-medium md:text-lg">
-        프로젝트 목표
-      </div>
+      <div className="text-sm font-medium md:text-lg">프로젝트 목표</div>
 
-      <div className="h-10 rounded-[10px] border border-gray-300 px-3 md:h-12 md:px-3.5">
+      <div className="focus-within:border-primary-500 h-10 rounded-[10px] border border-gray-300 px-3 md:h-12 md:px-3.5">
         <input
-          className="h-full w-full text-sm outline-none md:text-lg"
+          className="h-full w-full text-sm text-neutral-900 placeholder-gray-400 outline-none focus:placeholder-transparent md:text-lg"
           placeholder="프로젝트 목표를 입력해주세요"
           value={value}
           maxLength={maxLength}
